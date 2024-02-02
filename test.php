dd71271ad546ae77d0df9bfd0db20937177a57ec
Jacmiz <158401640+Jacmiz@users.noreply.github.com>
2024-02-02 21:35:28 +1100

    test2

--- test.php
+++ test.php
@@ -1,12 +1,7 @@
 <?php
 $dir = "/photos/";
 
-// Sort in ascending order - this is default
 $a = scandir($dir);
 
-// Sort in descending order
-$b = scandir($dir,1);
-
-print_r($a);
-print_r($b);
+echo $a;
 ?>
