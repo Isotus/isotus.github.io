72741494f9c977286c42274ddb53493124b5ad66
Jacmiz <158401640+Jacmiz@users.noreply.github.com>
2024-02-02 22:56:33 +1100

    changes

--- test.php
+++ test.php
@@ -4,9 +4,7 @@ $dir = "/photos/";
 $a = scandir($dir);
 
 $arrayLength = count($a);
-$i = 0
 for($i = 0; $i < $arrayLength; $i++){
-  print_r ("<img scr='"+$a[$i]+"'/>");
+  echo "<img src='" . $a[$i] . "'/>";
 }
-
 ?>
